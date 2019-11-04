623c339d1b7982ba378df1cc06f9f420e0512827
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-04 15:25:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 def introduction(name)
   puts "Hi, my name is Josh.\n"
 end
-def introduction_with_language("Dan", "Ember.js")
+def introduction_with_language(name, language)
   puts "Hi, my name is Dan and I am learning to program in Ember.js.\n"
 end
